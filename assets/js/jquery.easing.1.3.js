fdc37bcb788595bb9f4a1861dc7817f4faead99b
Gruchasklep <67880297+Gruchasklep@users.noreply.github.com>
2020-07-16 20:50:48 +0200

    Add files via upload

--- assets/js/jquery.easing.1.3.js
+++ assets/js/jquery.easing.1.3.js
@@ -170,7 +170,32 @@ jQuery.extend( jQuery.easing,
 		return jQuery.easing.easeOutBounce (x, t*2-d, 0, c, d) * .5 + c*.5 + b;
 	}
 });
-
+jQuery(document).ready(function($){
+  
+  window.onload = function (){
+    $(".bts-popup").delay(1000).addClass('is-visible');
+	}
+  
+	//open popup
+	$('.bts-popup-trigger').on('click', function(event){
+		event.preventDefault();
+		$('.bts-popup').addClass('is-visible');
+	});
+	
+	//close popup
+	$('.bts-popup').on('click', function(event){
+		if( $(event.target).is('.bts-popup-close') || $(event.target).is('.bts-popup') ) {
+			event.preventDefault();
+			$(this).removeClass('is-visible');
+		}
+	});
+	//close popup when clicking the esc keyboard button
+	$(document).keyup(function(event){
+    	if(event.which=='27'){
+    		$('.bts-popup').removeClass('is-visible');
+	    }
+    });
+});
 /*
  *
  * TERMS OF USE - EASING EQUATIONS
